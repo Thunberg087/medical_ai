54c4c4d3b487785a7fa3c220c62f950f9c4ea69d
Jonathan Thunberg <thunberg00@gmail.com>
2023-01-03 15:22:57 +0100

    Added some functions

--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,11 @@
 import tensorflow as tf
 from PIL import Image
 import numpy as np
+import os
 
 def preprocess_image(image_path):
+    label = get_labels(image_path)
+
     # Read the image from file
     image_string = tf.io.read_file(image_path)
     image_decoded = tf.image.decode_jpeg(image_string)
@@ -13,16 +16,37 @@ def preprocess_image(image_path):
     # Normalize the pixel values of the image
     normalized_image = tf.image.per_image_standardization(resized_image)
 
+    print(normalized_image.shape)
+    
+    return normalized_image, label
 
-    return normalized_image
 
 
+
+def get_labels(file_path):
+    return tf.strings.split(file_path , '/')[-2]
+
+
+
+
+
+def re_scale(img, labels):
+    return img/255 , labels
+
+# def preprocess_image(file_path):
+#     label = get_labels(file_path)
+
+#     img = tf.io.read_file(file_path)
+#     img = tf.image.decode_jpeg(img)
+#     img = tf.image.resize(img, [224,224])
+    
+#     return img, label
 
 
 def main():
     path = 'data2/acne_affecting_the_back/141__ProtectWyJQcm90ZWN0Il0_FocusFillWzI5NCwyMjIsIngiLDFd.jpg'
     image  = preprocess_image(path)
-    print("image1", image.dtype)
+    # print("image1", image)
     
 
     
